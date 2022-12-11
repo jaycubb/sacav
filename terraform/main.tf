9285b834735087d720b1555871d20f5653a4a45b
jaycubb <hartmanjb@proton.me>
2022-12-11 03:30:14 +0000

    set aside monitoring policy for now

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,53 +34,53 @@ resource "digitalocean_droplet" "web" {
   monitoring = true  # <-- enable monitoring for the compute instance
 }
 
-resource "digitalocean_monitoring_alert_policy" "policy" {
-  name   = "web-alert-policy"
-  region = "sea1"  # <-- specify region near Seattle
-
-  # create alerts for every possible metric
-  alert_rules = [
-    {
-      type = "metric"
-      name = "CPU usage"
-      value = "cpu"
-      period = "600"
-      threshold = "80"
-      operator = ">"
-    },
-    {
-      type = "metric"
-      name = "Disk I/O time"
-      value = "disk"
-      period = "600"
-      threshold = "1000"
-      operator = ">"
-    },
-    {
-      type = "metric"
-      name = "Memory usage"
-      value = "memory"
-      period = "600"
-      threshold = "80"
-      operator = ">"
-    },
-    {
-      type = "metric"
-      name = "Network in"
-      value = "net"
-      period = "600"
-      threshold = "10000000"
-      operator = ">"
-    },
-    {
-      type = "metric"
-      name = "Network out"
-      value = "net"
-      period = "600"
-      threshold = "10000000"
-      operator = "<"
-    },
-    {
-      type = "metric"
-      name = "Swap usage"
+#resource "digitalocean_monitoring_alert_policy" "policy" {
+#  name   = "web-alert-policy"
+#  region = "sea1"  # <-- specify region near Seattle
+#
+#  # create alerts for every possible metric
+#  alert_rules = [
+#    {
+#      type = "metric"
+#      name = "CPU usage"
+#      value = "cpu"
+#      period = "600"
+#      threshold = "80"
+#      operator = ">"
+#    },
+#    {
+#      type = "metric"
+#      name = "Disk I/O time"
+#      value = "disk"
+#      period = "600"
+#      threshold = "1000"
+#      operator = ">"
+#    },
+#    {
+#      type = "metric"
+#      name = "Memory usage"
+#      value = "memory"
+#      period = "600"
+#      threshold = "80"
+#      operator = ">"
+#    },
+#    {
+#      type = "metric"
+#      name = "Network in"
+#      value = "net"
+#      period = "600"
+#      threshold = "10000000"
+#      operator = ">"
+#    },
+#    {
+#      type = "metric"
+#      name = "Network out"
+#      value = "net"
+#      period = "600"
+#      threshold = "10000000"
+#      operator = "<"
+#    },
+#    {
+#      type = "metric"
+#      name = "Swap usage"
 
